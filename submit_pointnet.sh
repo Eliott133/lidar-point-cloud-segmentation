6742f0b71c9b35046f279750e4ec43151e5accc8
Eliott <mauberteliott9@gmail.com>
2026-03-11 10:20:13 +0100

    Add submit_pointnet.sh script for PointNet training setup

---
+++ submit_pointnet.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=pointnet_kitti_v2
+#SBATCH --output=logs/pointnet_kitti_%j.out
+#SBATCH --error=logs/pointnet_kitti_%j.err
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=80G
+#SBATCH --gres=gpu:1
+#SBATCH --time=150:00:00
+#SBATCH --partition=gpu
+#SBATCH --nodelist=gpue09
+
+cd ./PointNet
+
+# chargement module cuda
+module load cuda/12.4
+
+nvcc --version
+nvidia-smi
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+echo "Démarrage de l'entraînement via UV..."
+
+uv run --project PointNet torchrun --nproc_per_node=1 train_pointnet.py --config_path ../configs/config_pointnet.yaml
