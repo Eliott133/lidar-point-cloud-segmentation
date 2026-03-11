620e4942f97a857f7c3e51fd8f910f48b0f72f3b
Ahobade Josue Segla Hermion <s2405735@skinner.clusteretu.univ-lemans.fr>
2026-03-11 16:43:14 +0100

    update script sh

--- submit_randla_net.sh
+++ submit_randla_net.sh
@@ -21,11 +21,15 @@ echo "===================================="
 
 REPO_DIR=./RandLA-Net-pytorch
 
-echo "========= PYTHON INFO ========="
-uv run --active python -c "import sys; print('Python:', sys.executable)"
-uv run --active python -c "import torch; print('Torch:', torch.__version__); print('CUDA available:', torch.cuda.is_available())"
+cd "$REPO_DIR"
 
-echo "========= GPU INFO ========="
+export CUDA_HOME=/opt/cuda/12.4
+export PATH=$CUDA_HOME/bin:$PATH
+export LD_LIBRARY_PATH=$CUDA_HOME/lib64:$LD_LIBRARY_PATH
+
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+export PYTHONPATH=$PWD:$PYTHONPATH
+
 nvidia-smi
 
 echo "===================================="
@@ -35,14 +39,13 @@ echo "===================================="
 ls /info/raid-etu/m2/s2405735/pretrainned | head
 ls /info/raid-etu/m2/s2405735/pretrainned/sequences_0.06 | head
 
+uv run sh compile_op.sh
+
 echo "===================================="
 echo "STEP 2: START TRAINING"
 echo "===================================="
 
-uv run --project RandLA-Net-pytorch python RandLA-Net-pytorch/main_SemanticKITTI.py \
-  --log_dir logs \
-  --max_epoch 150 \
-  --batch_size 2
+uv run python main_SemanticKITTI.py --log_dir logs --max_epoch 150 --batch_size 2
 echo "===================================="
 echo "TRAINING FINISHED"
 echo "===================================="
